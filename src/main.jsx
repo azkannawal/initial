9702aa386f11effd7be967bc3b263ab3dcd8ce7c
azkannawal <azkannawalaf@gmail.com>
2023-11-20 19:37:05 +0700

    useReducer

--- src/main.jsx
+++ src/main.jsx
@@ -12,6 +12,7 @@ import { Provider } from "react-redux";
 import store from "./redux/store.js";
 import Navbar from "./component/Layouts/Navbar.jsx";
 import DarkModeContextProvider from "./context/DarkMode";
+import { TotalPriceProvider } from "./context/TotalPriceContext";
 
 const router = createBrowserRouter([
   {
@@ -49,7 +50,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <DarkModeContextProvider>
-        <RouterProvider router={router} />
+        <TotalPriceProvider>
+          <RouterProvider router={router} />
+        </TotalPriceProvider>
       </DarkModeContextProvider>
     </Provider>
   </React.StrictMode>
